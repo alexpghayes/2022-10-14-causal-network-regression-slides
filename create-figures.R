ace7c8a98226c9fca6eeacdc12d7af2a5ed79a4d
Alex Hayes <alexpghayes@gmail.com>
2022-10-14 12:27:18 -0500

    Update dump

--- create-figures.R
+++ create-figures.R
@@ -57,7 +57,7 @@ layout |>
   scale_size(guide = waiver()) +
   scale_color_brewer(direction = 1, palette = "RdYlGn") +
   labs(
-    title = "How to account for the social nature of drinking?",
+    title = "Alcohol use in the adolescent social network",
     subtitle = "Each node represents one adolescent",
     caption = "Source: Teenage Friends and Lifestyle Study, s129 dataset, 1995",
     color = "Frequency of\nalcohol use"
@@ -95,13 +95,50 @@ ggsave(
   dpi = 500
 )
 
+
 layout |>
   ggraph() +
   geom_edge_fan(
     arrow = arrow(length = unit(1, 'mm')),
     end_cap = circle(2.5, 'mm')
   ) +
-  geom_node_point(aes(color = tobacco_fct, size = in_degree)) +
+  geom_node_point(
+    aes(color = as.factor(friend_group), size = in_degree),
+    show.legend = FALSE
+  ) +
+  labs(
+    title = "Key idea: suppose node behavior varies with friend group",
+    subtitle = "i.e. nodal features (treatments, controls, outcomes) are homophilous",
+    caption = "Nodes colored by estimated friend group"
+  ) +
+  theme_void(base_size = 18, base_family = "Fira Sans")
+
+ggsave(
+  here::here("figures/glasgow-friendgroups.png"),
+  height = 6,
+  width = 6 * 13/9,
+  dpi = 500
+)
+
+sbm_pop <- model_block(n = 129, k = 13, expected_degree = 5)
+sbm_sample <- sample_tidygraph(sbm_pop) |>
+  mutate(
+    in_degree = centrality_degree(),
+    block = sbm_pop$mediator$A_model$z
+  )
+
+sbm_sample
+
+sbm_layout <- sbm_sample |>
+  create_layout(layout = "stress")
+
+sbm_layout |>
+  ggraph() +
+  geom_edge_fan(
+    arrow = arrow(length = unit(1, 'mm')),
+    end_cap = circle(2.5, 'mm')
+  ) +
+  geom_node_point(aes(color = block, size = in_degree)) +
   labs(
     title = "Friendships in a secondary school in Glasgow",
     subtitle = "s129 dataset from the Teenage Friends and Lifestyle Study, 1995",
@@ -111,71 +148,88 @@ layout |>
   ) +
   theme_void(base_size = 18, base_family = "Fira Sans")
 
+# library(gdim)
+#
+# A1 <- igraph::get.adjacency(glasgow1)
+#
+# cv_eigs <- eigcv(A1, k_max = 50)
+# cv_eigs
+#
+# plot(cv_eigs)  # suggests k ~= 4 or 5, maybe up to 9
+
+
+glasgow1 |>
+  filter(!is.na(alcohol_int), !is.na(money)) |>
+  sensitivity_curve(
+    drinks_dimaria ~ age + money,
+    max_rank = 15,
+    ranks_to_consider = 12
+  ) |>
+  plot() +
+  geom_vline(xintercept = 9, linetype = "dotted") +
+  scale_color_brewer(type = "qual") +
+  labs(
+    y = "Change in P(drinking at least monthly)"
+  ) +
+  theme_classic(base_size = 18, base_family = "Fira Sans")
+
 ggsave(
-  here::here("figures/glasgow-smokes.pdf")
+  here::here("figures/glasgow-estimates.png"),
+  height = 6,
+  width = 6 * 13/9,
+  dpi = 500
 )
-
-library(gdim)
-
-cv_eigs <- eigcv(A1, k_max = 50)
-cv_eigs
-```
-
-```{r}
-plot(cv_eigs)  # suggests k ~= 4 or 5, maybe up to 9
-```
-
-```{r}
-library(vsp)
-
-fa1 <- vsp(A1, rank = 9, degree_normalize = FALSE)
-fa1
-
-plot_mixing_matrix(fa1)
-screeplot(fa1)
-```
-
-
-```{r}
-plot_ipr_pairs(fa1)
-```
-
-
-```{r}
-library(tidyverse)
-library(vsp)
-
-fa1 %>%
-  get_varimax_y(1:9) %>%
-  dplyr::select(-id) %>%
-  GGally::ggpairs(ggplot2::aes(alpha = 0.001), progress = FALSE) +
-  ggplot2::theme_minimal()
-```
-
-
-```{r}
-fa1 %>%
-  get_varimax_z(1:9) %>%
-  dplyr::select(-id) %>%
-  dplyr::mutate(leverage = purrr::pmap_dbl(., sum)) %>%
-  dplyr::select(-leverage) %>%
-  GGally::ggpairs(ggplot2::aes(alpha = 0.001), progress = FALSE) +
-  ggplot2::theme_minimal()
-```
-looks like Y factors are better than Z factors
-
-```{r}
-V1 <- VS(A1, 9)
-
-m_fit <- nodelm(V1 ~ sex_fct, graph = glasgow1)
-anova(m_fit)
-
-o_fit <- nodelm(smokes_dimaria ~ sex_fct + V1, data = glasgow1)
-summary(o_fit)
-
-ols <- nodelm(smokes_dimaria ~ sex_fct, data = glasgow1)
-summary(ols)
-```
+#
+# library(vsp)
+#
+# fa1 <- vsp(A1, rank = 9, degree_normalize = FALSE)
+# fa1
+#
+# plot_mixing_matrix(fa1)
+# screeplot(fa1)
+# ```
+#
+#
+# ```{r}
+# plot_ipr_pairs(fa1)
+# ```
+#
+#
+# ```{r}
+# library(tidyverse)
+# library(vsp)
+#
+# fa1 %>%
+#   get_varimax_y(1:9) %>%
+#   dplyr::select(-id) %>%
+#   GGally::ggpairs(ggplot2::aes(alpha = 0.001), progress = FALSE) +
+#   ggplot2::theme_minimal()
+# ```
+#
+#
+# ```{r}
+# fa1 %>%
+#   get_varimax_z(1:9) %>%
+#   dplyr::select(-id) %>%
+#   dplyr::mutate(leverage = purrr::pmap_dbl(., sum)) %>%
+#   dplyr::select(-leverage) %>%
+#   GGally::ggpairs(ggplot2::aes(alpha = 0.001), progress = FALSE) +
+#   ggplot2::theme_minimal()
+# ```
+# looks like Y factors are better than Z factors
+#
+# ```{r}
+# V1 <- VS(A1, 9)
+#
+# m_fit <- nodelm(V1 ~ sex_fct, graph = glasgow1)
+# anova(m_fit)
+#
+# o_fit <- nodelm(smokes_dimaria ~ sex_fct + V1, data = glasgow1)
+# summary(o_fit)
+#
+# ols <- nodelm(smokes_dimaria ~ sex_fct, data = glasgow1)
+# summary(ols)
+# ```
 
 
 
